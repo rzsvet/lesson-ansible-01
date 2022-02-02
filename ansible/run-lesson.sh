61330cbf179147770afe8d28917f40b7e338fa30
Aleksey Tsvetkov <svet@rzhev.me>
2022-02-02 13:36:30 +0300

    Add task for modify grub

--- ansible/run-lesson.sh
+++ ansible/run-lesson.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "Start test playbook with Ansible-Lint"
-ansible-lint main.yml
 FILE=./playbook-settings-password
 if [ -f "$FILE" ]; then
     ansible-playbook main.yml --vault-password-file playbook-settings-password
